bfc7dcb8a0a4b3a7de25a9622e2620ae3aa54dda
Etienne Camenen <etienne.camenen@gmail.com>
2018-03-28 14:22:02 +0200

    Changed version to 2.0

--- src/main/java/fr/inra/toulouse/metexplore/launcher/Launcher.java
+++ src/main/java/fr/inra/toulouse/metexplore/launcher/Launcher.java
@@ -50,7 +50,7 @@ public abstract class Launcher implements WritingComportment{
         }
 
         if (this.version) {
-            System.out.println("Version: 1.1");
+            System.out.println("Version: 2.0");
             exit(0);
         }
     }
